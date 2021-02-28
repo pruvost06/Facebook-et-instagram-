6d8a9b7bd60ab5018d5d11559b8e97e80a80ff07
pruvost06 <rolfpruvost@gmail.com>
2021-03-01 00:46:41 +0100

    js-dossier

---
+++ js/min.js
@@ -0,0 +1,28 @@
+
+
+
+$(document).ready(function(){
+$(".btn1").mouseenter(function(){
+  $("body").css("background", "-webkit-linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%"); 
+ });
+ $(".btn1").mouseleave(function(){
+  $("body").css("background", "white")
+});
+$(".btn2").mouseenter(function(){
+  
+  $("body").css("background", "  #0092ed  "); 
+
+});
+$(".btn2").mouseleave(function(){
+$("body").css("background", "white")
+});
+});
+
+//----------------jquery2---------------------------------------------//
+
+$(document).ready(function(){
+
+  $(".bouton").mouseenter(function(){
+    $("body").css("background", "-webkit-linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%"); 
+   });
+  });
